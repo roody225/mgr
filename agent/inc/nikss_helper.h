a7ad08efce10e0fb844d8ccf25805e07cc2c661c
Andrzej Tkaczyk <andrzej.tkaczyk@codilime.com>
2023-05-12 01:06:28 +0200

    initial nikss add or update implementation

---
+++ agent/inc/nikss_helper.h
@@ -0,0 +1,43 @@
+#ifndef _NIKSS_HELPER_H_
+#define _NIKSS_HELPER_H_
+
+#include <linux/if_ether.h>
+#include <nikss/nikss.h>
+
+enum key_type {
+    IP4_ADDR,
+    IP4_LPM,
+    U_32,
+};
+
+struct match_elem {
+    enum key_type type;
+    uint32_t val;
+    uint8_t prefix_len;
+};
+
+enum data_type {
+    IP4_DATA,
+    U_32_DATA,
+    MAC_DATA,
+};
+
+struct data_elem {
+    enum data_type type;
+    uint32_t val;
+    uint8_t mac[ETH_ALEN];
+};
+
+struct entry_spec {
+    nikss_pipeline_id_t pipe;
+    const char *table_name;
+    const char *action_name;
+    struct match_elem *elems;
+    uint8_t num_elems;
+    struct data_elem *data;
+    uint8_t num_data;
+};
+
+int table_entry_add(struct entry_spec *entry);
+
+#endif /* _NIKSS_HELPER_H_ */
--- agent/src/p4_handlers.c
+++ agent/src/p4_handlers.c
@@ -1,13 +1,34 @@
 #include <stdio.h>
+#include <string.h>
 
 #include <linux/rtnetlink.h>
 #include <arpa/inet.h>
 
 #include "p4_handlers.h"
+#include "nikss_helper.h"
 
 int insert_newlink(struct link_message *lm)
 {
+    struct entry_spec spec;
+    struct match_elem key;
+    struct data_elem action_data;
     printf("INFO: new link %d table %d\n", lm->id, (lm->is_vrf_slave ? lm->vrf : RT_TABLE_MAIN));
+
+    spec.pipe = 0;
+    spec.table_name = "ingress_vrf_table";
+    spec.action_name = "ingress_set_vrf";
+    spec.elems = &key;
+    spec.num_elems = 1;
+    spec.data = &action_data;
+    spec.num_data = 1;
+
+    key.val = lm->id;
+    key.type = U_32;
+
+    action_data.val = lm->is_vrf_slave ? lm->vrf : RT_TABLE_MAIN;
+    action_data.type = U_32_DATA;
+
+    table_entry_add(&spec);
     return 0;
 }
 int insert_dellink(struct link_message *lm)
@@ -18,6 +39,9 @@ int insert_dellink(struct link_message *lm)
 
 int insert_newroute(struct route_message *rm)
 {
+    struct entry_spec spec;
+    struct match_elem key[2];
+    struct data_elem action_data[2];
     const size_t IP_BUF_SIZE = 20;
 	char addr[IP_BUF_SIZE], gw[IP_BUF_SIZE];
 
@@ -27,6 +51,38 @@ int insert_newroute(struct route_message *rm)
     printf("INFO: new route %s/%d dev %d gw %s table %d\n",
         addr, rm->prefix_len, rm->dev, gw, rm->vrf);
 
+    spec.pipe = 0;
+    spec.elems = key;
+    spec.data = action_data;
+    if (rm->vrf == RT_TABLE_LOCAL) {
+        spec.table_name = "ingress_forward_local_table_ip4";
+        spec.action_name = "ingress_normal_path_to_kernel";
+        spec.num_elems = 1;
+        spec.num_data = 0;
+
+        key[0].val = htonl(rm->addr);
+        key[0].type = IP4_ADDR;
+    } else if ((rm->gw == 0) || (rm->dev == 0)) {
+        return 0;
+    } else {
+        spec.table_name = "ingress_routing_table_ip4";
+        spec.action_name = "ingress_forward_ip4";
+        spec.num_elems = 2;
+        spec.num_data = 2;
+
+        key[0].val = rm->vrf;
+        key[0].type = U_32;
+        key[1].val = htonl(rm->addr);
+        key[1].prefix_len = rm->prefix_len;
+        key[1].type = IP4_LPM;
+
+        action_data[0].val = rm->dev;
+        action_data[0].type = U_32_DATA;
+        action_data[1].val = htonl(rm->gw);
+        action_data[1].type = IP4_DATA;
+    }
+
+    table_entry_add(&spec);
     return 0;
 }
 int insert_delroute(struct route_message *rm)
@@ -45,6 +101,9 @@ int insert_delroute(struct route_message *rm)
 
 int insert_newneigh(struct neigh_message *nm)
 {
+    struct entry_spec spec;
+    struct match_elem key;
+    struct data_elem action_data;
     const size_t IP_BUF_SIZE = 20;
 	char gw[IP_BUF_SIZE];
 
@@ -53,6 +112,26 @@ int insert_newneigh(struct neigh_message *nm)
     printf("INFO: new neigh %02x:%02x:%02x:%02x:%02x:%02x gw %s\n",
         nm->addr[0], nm->addr[1], nm->addr[2], nm->addr[3], nm->addr[4], nm->addr[5], gw);
 
+    spec.pipe = 0;
+    spec.table_name = "ingress_arp_resolve_ip4";
+    spec.action_name = "ingress_set_dst_mac";
+    spec.elems = &key;
+    spec.num_elems = 1;
+    spec.data = &action_data;
+    spec.num_data = 1;
+
+    key.val = htonl(nm->gw);
+    key.type = IP4_ADDR;
+
+    action_data.mac[0] = nm->addr[5];
+    action_data.mac[1] = nm->addr[4];
+    action_data.mac[2] = nm->addr[3];
+    action_data.mac[3] = nm->addr[2];
+    action_data.mac[4] = nm->addr[1];
+    action_data.mac[5] = nm->addr[0];
+    action_data.type = MAC_DATA;
+
+    table_entry_add(&spec);
     return 0;
 }
 int insert_delneigh(struct neigh_message *nm)
